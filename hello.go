78f7731e109efd1e7b5a2cdc692a99c6ae14ec02
Jedsada Luengaramsuk <68596277+jedsada-io@users.noreply.github.com>
2021-10-09 08:46:48 +0100

    FIrst class

---
+++ hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world!")
+}
+
+//to-run
+// go run go-hello-world.go
---
+++ to-install-go-app.go
@@ -0,0 +1,6 @@
+$ go install github.com/rakyll/hey@latest
+go: downloading github.com/rakyll/hey v0.1.4
+go: downloading golang.org/x/net v0.0.0-20181017193950-04a2e542c03f
+go: downloading golang.org/x/text v0.3.0 
+
+Bodner, Jon. Learning Go (p. 14). O'Reilly Media. Kindle Edition. 
