ce2a42d03a88b5a3a6ad373d0f6822bdb0ec439d
Jedsada Luengaramsuk <68596277+jedsada-io@users.noreply.github.com>
2022-02-01 17:39:04 +0000

    Week4 Tasks

--- PFDA/Week3/Seminar Solution/tableu.py
+++ PFDA/Week3/Seminar Solution/tableu.py
@@ -2,3 +2,15 @@ adjustments = []
 for i in range(len(Sales)):
 adjustment = Profit[i] – Sales[i] * rate
 adjustments.append(adjustment) return adjustments
+
+
+SCRIPT_REAL(
+"
+adjustments = []
+for i in range(len(_arg1)):
+adjustment = _arg2[i] - _arg1[i]*_arg3[0]
+adjustments.append(adjustment) return adjustments
+",
+SUM([Sales]),
+SUM([Profit]),
+[rate]
